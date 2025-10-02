2a8a743849abf7ac82b2e0ff5aa5f1b0785c426f
depst-10538 <tur12015@temple.edu>
2025-10-02 14:47:23 -0400

    Accepted value from text size selector to change display

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -5,9 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val SIZE_KEY = "SIZE_KEY"
 
 class DisplayActivity : AppCompatActivity() {
 
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            it.data?.apply{
+                // set text size equal to selected value
+                lyricsDisplayTextView.textSize = getIntExtra(SIZE_KEY,22).toFloat()
+            }
+        }
+    }
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
 
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
@@ -25,7 +36,7 @@ class DisplayActivity : AppCompatActivity() {
         // open selector on button click
         textSizeSelectorButton.setOnClickListener {
             val intent = Intent(this@DisplayActivity,TextSizeActivity::class.java)
-            startActivity(intent)
+            launcher.launch(intent)
         }
     }
 }
--- app/src/main/java/edu/temple/activities/TextSizeActivity.kt
+++ app/src/main/java/edu/temple/activities/TextSizeActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,7 +25,13 @@ class TextSizeActivity : AppCompatActivity() {
 
             // TODO Step 2: Pass selected value back to activity that launched TextSizeActivity
             adapter = TextSizeAdapter(textSizes){
-
+                item: Int ->
+                // return the selected data back
+                val intent = Intent().apply{
+                    putExtra(SIZE_KEY,item)
+                }
+                setResult(RESULT_OK,intent)
+                finish()
             }
             layoutManager = LinearLayoutManager(this@TextSizeActivity)
         }
